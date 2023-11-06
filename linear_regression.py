bd19d2c52b966514480e5a59ec11fa14d535700b
SanneJolijn <131674566+SanneJolijn@users.noreply.github.com>
2023-11-06 09:34:55 +0100

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,4 @@
-#%%
+# Import necessary packages and files
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import (mean_squared_error,
@@ -21,20 +21,23 @@ from sklearn.svm import SVR
 import csv
 from utils import import_xlsx_data
 
-#%%
+# Define a function for linear regression
 def linear_regression(n_outer, random_state, X, y):
+    # Configure the cross-validation procedure
     outer_cv = KFold(n_splits=n_outer, shuffle=True, random_state=random_state)
     outer_results = list()
 
     df_metrics = pd.DataFrame()
 
     for train_ix, test_ix in outer_cv.split(X):
-        # split data
+        # Split data
         X_train, X_test = X.loc[train_ix], X.loc[test_ix]
         y_train, y_test = y.loc[train_ix], y.loc[test_ix]
 
+        # Create a linear regression model
         model = LinearRegression().fit(X_train, y_train)
 
+        # Get model parameters and metrics
         itc = model.intercept_
         coef = model.coef_
 
@@ -51,37 +54,28 @@ def linear_regression(n_outer, random_state, X, y):
 
     return df_metrics
 
-
-#%%
 #Determine all variables
 np.random.seed(12)
 random_state = 12
 n_outer = 10
 
-
-# %%
-#Import our data
+#Import data
 data = import_xlsx_data('upy AA input.xlsx')
 df = data.drop(data.columns[0], axis=1)
 X, y = df.drop('Turbidity', axis=1), df[['Turbidity']]
 
-#%%
 #Perform the linear regression on all compounds and turbidity
 df_performance_LR = linear_regression(n_outer, random_state, X, y)
 df_performance_LR.to_csv("regression performance.csv") 
 
-#%%
 #Perform the linear regression on c4 and turbidity
 X1, y1 = df[['c4']], df[['Turbidity']]
 df_performance_c4 = linear_regression(n_outer, random_state, X1, y1)
 df_performance_c4.to_csv("regression c4 performance.csv") 
 
-# %%
 #Ablation studies
 data2 = import_xlsx_data('upy AA input 3.xlsx')
 df = data2.drop(data2.columns[0], axis=1)
 X2, y2 = df.drop('Turbidity', axis=1), df[['Turbidity']]
-
-#%%
 df_performance_LR2 = linear_regression(n_outer, random_state, X2, y2)
 df_performance_LR2.to_csv("regression ablation.csv") 
